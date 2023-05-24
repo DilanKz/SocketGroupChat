22be966bb5694dad3ff474dc9d061a8421866723
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-05-24 21:41:10 +0530

    Clients added

---
+++ JavaSocketGroupChat/Client2/src/Client2.java
@@ -0,0 +1,49 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client2 {
+    public static void main(String[] args) {
+        final String serverIP = "SERVER_IP_ADDRESS"; // Replace with the server's IP address
+        final int port = 8000; // Specify the port number you want to use
+
+        try{
+
+            Socket socket = new Socket("localhost",port);
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            Scanner scanner=new Scanner(System.in);
+
+            System.out.print("Enter your username: ");
+            String name = scanner.nextLine();
+            dataOutputStream.writeUTF(name);
+            dataOutputStream.flush();
+
+
+
+            while (true){
+                System.out.print("Enter Message: ");
+                String msg = scanner.nextLine();
+
+                if (msg.equalsIgnoreCase("CLOSE")){
+                    System.out.println("Chat Closed");
+                    break;
+                }
+
+                dataOutputStream.writeUTF(msg);
+                dataOutputStream.flush();
+                System.out.println(dataInputStream.readUTF());
+
+
+            }
+
+            dataOutputStream.close();
+            socket.close();
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
